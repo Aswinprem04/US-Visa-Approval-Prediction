26986ff8a7d3591b6c8ede20175b13a7a86acb50
Aswin <aswinprem69@gmail.com>
2024-08-11 20:25:43 +0530

    Upto Model trainer

--- demo.py
+++ demo.py
@@ -1,13 +1,4 @@
-# from us_visa.pipline.training_pipeline import TrainPipeline
+from us_visa.pipline.training_pipeline import TrainPipeline
 
-# obj = TrainPipeline()
-# obj.run_pipeline()
-
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-
-#logging.info("welcome to my custom log")
-try:
-    a = 2/0
-except Exception as e:
-    raise USvisaException(e,sys)
+obj = TrainPipeline()
+obj.run_pipeline()
